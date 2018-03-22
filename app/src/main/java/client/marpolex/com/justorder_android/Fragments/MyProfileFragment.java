3d69fb7ad8b4788e8cc8099e0424bbedd82f617e
Mario Ramos Montesinos <mariodarken@gmail.com>
2018-03-22 20:43:58 +0100

    NavBar funcional

---
+++ app/src/main/java/client/marpolex/com/justorder_android/Fragments/MyProfileFragment.java
@@ -0,0 +1,24 @@
+package client.marpolex.com.justorder_android;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mario on 22/03/2018.
+ */
+
+public class MyProfileFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.myprofile, container, false);
+        return myView;
+    }
+}
