e71138cbfb0b7676081d23ce07def7efdbd261f4
Mario Ramos Montesinos <mariodarken@gmail.com>
2018-05-05 19:37:54 +0200

    Toolbar menu

--- app/src/main/java/client/marpolex/com/justorder_android/Activities/Carta/MenuActivity.java
+++ app/src/main/java/client/marpolex/com/justorder_android/Activities/Carta/MenuActivity.java
@@ -1,9 +1,13 @@
 package client.marpolex.com.justorder_android.Activities.Carta;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -21,27 +25,27 @@ import client.marpolex.com.justorder_android.Adapters.CategoriesAdapter;
 import client.marpolex.com.justorder_android.Models.Category;
 import client.marpolex.com.justorder_android.R;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity  {
 
     List<Category> categoryList;
     int idRestaurant;
     String nameRestaurant = "Restaurant test";
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        //TOOLBAR
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);  // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar); // Setting toolbar as the ActionBar with setSupportActionBar() call
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setSubtitleTextColor(Color.WHITE);
+        //END TOOLBAR
+
         idRestaurant = (int) getIntent().getLongExtra("idRestaurant", 0);
         categoryList = loadData(idRestaurant); //Toda la carta
-
-        /*Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mActionBarToolbar);
-        setSu
-        getSupportActionBar().setTitle("My title");
-
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
-        getSupportActionBar().setTitle(nameRestaurant);*/
 
         final CategoriesAdapter cAdapter = new CategoriesAdapter(this, categoryList);
         final GridView lstElements = (GridView) findViewById(R.id.gvCategory);
@@ -62,14 +66,36 @@ public class MenuActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     private List<Category> loadData(int idRestaurant) { //TODO cargar id Restaurant
         List<Category> categoryList = new ArrayList<Category>();
-        Log.d("carta", "loadData: Cargando carta del restaurante "+idRestaurant);
+        Log.d("carta", "loadData: Cargando carta del restaurante " + idRestaurant);
 
         //Cargar JSON
         String json = null;
         try {
-            InputStream is = getAssets().open("Restaurant_"+idRestaurant+"_cart.json");
+            InputStream is = getAssets().open("Restaurant_" + idRestaurant + "_cart.json");
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
